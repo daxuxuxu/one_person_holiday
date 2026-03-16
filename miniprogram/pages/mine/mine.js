1d0db57b2f069ff11aca2cd9ae4df7ad29b69318
daxuxuxu <40338311+daxuxuxu@users.noreply.github.com>
2026-03-16 22:40:06 +0800

    Update mine.js

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -1,14 +1,35 @@
+// miniprogram/pages/mine/mine.js
 const { getRoutes, getFavorites } = require('../../utils/store');
 
 Page({
   data: {
+    user: null,
     favRoutes: []
   },
 
   onShow() {
+    // 登录 / 刷新用户信息
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {
+        nickName: '微信用户',
+        avatarUrl: '',
+        ageRange: '26-30',
+        incomeRange: '8k-20k'
+      }
+    }).then(res => {
+      console.log('login result:', res.result);
+      const user = res.result.user;
+      wx.setStorageSync('currentUser', user);
+      this.setData({ user });
+    }).catch(err => {
+      console.error('login failed:', err);
+    });
+
+    // 加载收藏列表
     const all = getRoutes();
     const favIds = getFavorites();
-    const favRoutes = all.filter((r) => favIds.includes(r.id));
+    const favRoutes = all.filter(r => favIds.includes(r.id));
     this.setData({ favRoutes });
   },
 
