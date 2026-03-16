ae1d04b25e325b0b66599af625ce7df9d689a858
daxuxuxu <40338311+daxuxuxu@users.noreply.github.com>
2026-03-16 22:20:16 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,12 @@
-const { initSeed } = require('./utils/store');
-
 App({
   onLaunch() {
-    initSeed();
+    if (!wx.cloud) {
+      console.error("请使用 2.2.3 及以上基础库");
+      return;
+    }
+    wx.cloud.init({
+      env: "你的云环境ID", // 例如 cloud1-xxxx
+      traceUser: true
+    });
   }
 });
